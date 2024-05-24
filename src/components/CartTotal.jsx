ba373e38bd8ce80f5aaf715b30b3b15761165f85
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2024-05-24 18:56:24 +0300

    fix: Round total and subTotal price in CartTotal component

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,17 +1,20 @@
 import { useTranslation } from "react-i18next";
 
 export function CartTotal({ subtotal, shipping, total }) {
+  console.log(Math.round(subtotal));
   const { t } = useTranslation();
   return (
     <>
       <p className="total">
-        {t("Subtotal")}: <span>${subtotal}</span>
+        {t("Subtotal")}:{" "}
+        <span>${Math.round((subtotal + Number.EPSILON) * 100) / 100}</span>
       </p>
       <p className="shipping">
         {t("Shipping")}: <span>{t(shipping)}</span>
       </p>
       <p className="total">
-        {t("Total")}: <span>${total}</span>
+        {t("Total")}:{" "}
+        <span>${Math.round((total + Number.EPSILON) * 100) / 100}</span>
       </p>
     </>
   );
